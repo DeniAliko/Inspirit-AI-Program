d1bc681d86663830a91873d57ce3b2d0f13f12bd
DeniAliko <denialiko2016@gmail.com>
2023-07-03 17:51:46 -0400

    day 1 tomfoolery

---
+++ linearregressions.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("car_dekho.csv")
+print(data.head())
+# print(data[['Fuel_Type']].head())
+# print(data[["Car_Name"]].head())
+
+sns.scatterplot(y = "Selling_Price", x = "Age", data = data)
+# plt.show()
+sns.catplot(x = 'Fuel_Type', y = 'Selling_Price', data = data, kind = 'swarm', s = 2)
+# plt.show()
+
+print(data.groupby(["Fuel_Type"]).count())
+
+sns.scatterplot(x = "Kms_Driven", y = "Selling_Price", data = data)
+plt.show()
+sns.catplot(x = 'Seller_Type', y = 'Transmission', data = data, kind = 'swarm', s = 2)
+plt.show()
