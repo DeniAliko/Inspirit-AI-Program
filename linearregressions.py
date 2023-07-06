66b00689be7bde892a96cc75f6449ea3bd9f592e
DeniAliko <denialiko2016@gmail.com>
2023-07-06 14:55:40 -0400

    import sklearn not working lol

--- linearregressions.py
+++ linearregressions.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sklearn as sklearn
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 data = pd.read_csv("car_dekho.csv")
 print(data.head())
@@ -18,3 +21,8 @@ sns.scatterplot(x = "Kms_Driven", y = "Selling_Price", data = data)
 plt.show()
 sns.catplot(x = 'Seller_Type', y = 'Transmission', data = data, kind = 'swarm', s = 2)
 plt.show()
+
+xAge = data["Age"]
+yPrice = data["Selling_Price"]
+print(xAge)
+print(yPrice)
